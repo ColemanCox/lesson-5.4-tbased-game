130f9a1bce1d3146db7bd8895d0386e896283f21
Coleman Cox <colezane82@gmail.com>
2015-08-10 16:39:58 -0500

    yeah, baaaaby

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+var AppTemplates = {};
+
+AppTemplates['app'] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true});//# sourceMappingURL=app.map
